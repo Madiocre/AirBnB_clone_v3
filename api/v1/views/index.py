642681fb786b93249172fbc1ec64a546b21ec28c
Madiocre <wolvirex@gmail.com>
2024-04-30 16:19:29 -0700

    added style and index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,13 +15,13 @@ def api_status():
     """  Returns JSON'd Text  """
     return jsonify(status='OK')
 
+
 @app_views.route("/stats")
 def something():
     """ JSON """
     data = {'states': State, 'users': User,
             'amenities': Amenity, 'cities': City,
             'places': Place, 'reviews': Review}
-    
     for item in data:
         data[item] = storage.count(data[item])
     return jsonify(data)

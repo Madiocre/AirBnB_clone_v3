7f59da0775bf963e6f95989ef4500781425b454e
madiocre <wolvirex@gmail.com>
2024-05-01 02:44:53 +0300

    attempted fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,6 +19,12 @@ def downtear(self):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
+
+
 if __name__ == "__main__":
     HOST = getenv("HBNB_API_HOST", "0,0,0,0")
     PORT = int(getenv("HBNB_API_PORT", 5000))

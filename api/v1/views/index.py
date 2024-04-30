47678604f81dca8a47ae1df01049b82a97e52b42
madiocre <wolvirex@gmail.com>
2024-05-01 02:24:06 +0300

    attempted fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,8 +6,10 @@ starts a Flask web application
 import models
 from models import storage
 from models.base_model import BaseModel
-from flask import jsonify
+from flask import Flask, jsonify
 from api.v1.views import app_views
+
+app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 

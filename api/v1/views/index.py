6f19583ac56cf6ed96adee2dfed656663ecfa6ed
madiocre <wolvirex@gmail.com>
2024-05-01 02:08:28 +0300

    added index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,9 @@
 starts a Flask web application
 """
 
-
+import models
+from models import storage
+from models.base_model import BaseModel
 from flask import jsonify
 from api.v1.views import app_views
 
@@ -12,3 +14,14 @@ from api.v1.views import app_views
 def api_status():
     """  Returns JSON'd Text  """
     return jsonify(status='OK')
+
+@app_views.route("/stats")
+def something():
+    """ JSON """
+    data = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    
+    for item in data:
+        data[item] = storage.count(data[item])
+    return jsonify(data)

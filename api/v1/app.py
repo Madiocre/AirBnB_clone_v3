014c2a0a7e15fad76e2bcf1b5a36fd06bf18a209
madiocre <wolvirex@gmail.com>
2024-05-01 01:06:09 +0300

    pushed?

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from os import getenv
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
@@ -14,4 +15,4 @@ app.register_blueprint(app_views)
 if __name__ == "__main__":
     HOST = getenv("HBNB_API_HOST", "0,0,0,0")
     PORT = int(getenv("HBNB_API_PORT", 5000))
-    app.run(hosts = HOST, port = PORT, threaded=True)
+    app.run(hosts=HOST, port=PORT, threaded=True)

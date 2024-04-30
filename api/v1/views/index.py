7881b3af7f34cc56b541ccb63f7a872c2a358187
Madiocre <wolvirex@gmail.com>
2024-04-30 15:56:00 -0700

    attempted fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,12 @@
 starts a Flask web application
 """
 
+
 from flask import jsonify
 from api.v1.views import app_views
 
-@app_views.route("/status") 
-def api_status(): 
+
+@app_views.route("/status")
+def api_status():
     """  Returns JSON'd Text  """
-    return jsonify(status = 'OK')
+    return jsonify(status='OK')

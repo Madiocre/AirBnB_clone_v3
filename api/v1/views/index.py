b7971854e1a3a2d82e7c30371e810a1f2242eda7
madiocre <wolvirex@gmail.com>
2024-05-01 14:18:42 +0300

    index attempt fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -22,11 +22,9 @@ def api_status():
 @app_views.route("/stats")
 def get_stats():
     """ JSON """
-    data = {'amenities': storage.count('Amenity'),
-            'cities': storage.count('City'),
-            'places': storage.count('Place'),
-            'reviews': storage.count('Review'),
-            'states': storage.count('State'),
-            'users': storage.count('User')
-            }
+    data = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    for key in data:
+        data[key] = storage.count(data[key])
     return jsonify(data)

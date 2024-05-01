ee39b5e7601e6a288d14aa0b2c0fb8c9bfeced49
madiocre <wolvirex@gmail.com>
2024-05-01 23:02:06 +0300

    TestRun

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,21 +6,19 @@ starts a Flask web application
 from models.engine.db_storage import classe
 from models import storage
 from models.base_model import BaseModel
-from flask import Flask, jsonify
+from flask import jsonify
 from api.v1.views import app_views
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app_views.route("/status")
+@app_views.route("/status", strict_slashes=False)
 def api_status():
     """  Returns JSON'd Text  """
     return jsonify(status='OK')
 
 
-@app_views.route("/stats")
-def get_stats():
+@app_views.route("/stats", strict_slashes=False)
+def stats():
     """ JSON """
     c = classe
     for clas in c:

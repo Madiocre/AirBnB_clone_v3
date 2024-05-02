3419ad6519665207920f2d0316600e1b5a986838
madiocre <wolvirex@gmail.com>
2024-05-02 18:38:59 +0300

    added cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,11 +7,13 @@ from os import getenv
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
-
+CORS(app)
 
 @app.teardown_appcontext
 def downtear(self):
